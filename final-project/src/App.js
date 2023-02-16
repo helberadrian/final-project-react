6e2eca89c4cd1d6493c5fec69db336834852dde0
helberadrian <helberadrian@gmail.com>
2023-02-16 17:58:33 -0300

    pre entrega 2 1.1

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import NavBar from './components/navbar';
-import CartWidget from './components/cartWidget';
-
 
 // Pages
 import Home from "./pages/home/index";
@@ -15,10 +13,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <div className="App-header">
-            <NavBar />
-            <CartWidget number={0}></CartWidget>
-          </div>
+          <NavBar />
           <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/category/:categoryId' element={<Category />} />
--- final-project/src/components/navbar/index.js
+++ final-project/src/components/navbar/index.js
@@ -1,35 +1,27 @@
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 import logo from './media/logo.png';
 
+import CartWidget from '../cartWidget/index';
+
 const NavBar = () => {
     return (
-        <div className="navbar">
+        <nav className="navbar">
             <img src={logo} alt="Logo"/>
             <ul className='menu'>
-                <NavLink
-                    to="/"
-                    className="NavLink"
-                    style={({ isActive }) => (isActive ? activeStyle : undefined)}
-                    >
-                    Inicio
-                </NavLink>
-                <NavLink
-                    to="/category"
-                    className="NavLink"
-                    style={({ isActive }) => (isActive ? activeStyle : undefined)}
-                    >
-                    Categorias
-                </NavLink>
-                <NavLink
-                    to="/contact"
-                    className="NavLink"
-                    style={({ isActive }) => (isActive ? activeStyle : undefined)}
-                    >
-                    Contact
-                </NavLink>
+                <li>
+                    <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                    <Link to="/category">Categoria</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contacto</Link>
+                </li>
             </ul>
-        </div>
+            <CartWidget number={0}></CartWidget>
+        </nav>
     );
 }
 
